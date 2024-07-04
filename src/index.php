349ce5a4c01caf776b9e7e6b1de5582ca4ab4490
AlbertJun10r <albertjunior554@gmail.com>
2024-07-04 14:27:15 -0400

    Inicializar proyecto Hola Mundo con Apache, PHP y MySQL usando Docker Compose

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "mysql";
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully to MySQL.<br>";
+
+echo "Hola Mundo desde PHP!";
+?>
